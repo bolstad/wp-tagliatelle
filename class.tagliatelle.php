f377b4bcfb8964cce37ddaf2745cefceee57aa88
Christian Bolstad <kers@atdt.nu>
2011-05-29 19:56:58 +0200

    now in a class + debug function

--- class.tagliatelle.php
+++ class.tagliatelle.php
@@ -11,11 +11,21 @@ Version: 1.0
 Author URI: http://christianbolstad.se/
 */
 
-
-  function find_tagged_post($tag, $value)
+class Tagliatelle
+{
+  
+  public $debug = 0;
+  
+  function debugmsg($str)
+  {
+    if ($this->debug) echo "$str<br>\n"; 
+  }
+  
+  
+  function find_tagged_post($tag, $value,$post_type = 'post')
   {
       $superid = null;
-      query_posts(array('posts_per_page' => '1', 'post_type' => 'product', 'meta_key' => $tag, 'meta_value' => $value, 'orderby' => 'meta_value_num', 'order' => 'DESC'));
+      query_posts(array('posts_per_page' => '1', 'post_type' => $post_type, 'meta_key' => $tag, 'meta_value' => $value, 'orderby' => 'meta_value_num', 'order' => 'DESC'));
       if (have_posts())
           : while (have_posts())
           : the_post();
@@ -33,59 +43,61 @@ Author URI: http://christianbolstad.se/
           $rs = wp_get_object_terms($postid, 'post_tag');
           foreach ($rs as $item) {
               if ($termid == $item->term_id) {
-                  print "Tag '$tag' ($termid) is already connected to Page $postid<br>\n";
+                  $this->debugmsg("Tag '$tag' ($termid) is already connected to Page $postid");
                   return true;
               }
           }
       } else {
-          print "taggen '$tag' finns inte\n";
+          $this->debugmsg("taggen '$tag' finns inte");
           return false;
       }
   }
   
-  function write_tags($id, $tags, $taxonomy)
+  function write_tags($id, $tags, $taxonomy = 'post_tag')
   {
       $tags = explode(',', $tags);
       foreach ($tags as $solotag) {
           $solotag = trim($solotag);
-          print "Solotag $solotag <br>\n";
-          if (tagConnected($solotag, $id)) {
-              print "- PASSIVE: '$solotag' is already connected with '$id'<br>\n";
+          if ($this->debug) $this->debugmsg("Solotag $solotag <br>\n");
+          if ($this->tagConnected($solotag, $id)) {
+              $this->debugmsg("- PASSIVE: '$solotag' is already connected with '$id'");
           } else {
-              print "- ACTIVE: '$solotag' is NOT already connected with '$id'<br>\n";
-              print "  Testar $solotag<br>\n";
+              $this->debugmsg("- ACTIVE: '$solotag' is NOT already connected with '$id'");
+              $this->debugmsg("  Testar $solotag<br>");
               $check = is_term($solotag, $taxonomy);
               if (is_null($check)) {
-                  print "  '$solotag' finns inte som solotag'\n<br>";
+                  $this->debugmsg("  '$solotag' finns inte som solotag'");;
                   $tag = wp_insert_term($solotag, $taxonomy);
                   if (!is_wp_error($tag)) {
                       $tagid = $tag['term_id'];
-                      print "  Lyckades stoppa in '$solotag' som en tag med id  '$tagid'\n<br>";
+                      $this->debugmsg("  Lyckades stoppa in '$solotag' som en tag med id  '$tagid'");
                   } else {
                       //$tagid = $check['term_id'];
-                      print "error BEEP BEEP - could not create the tag '$tag' \n";
-                      print_r($check);
+                      $this->debugmsg("error BEEP BEEP - could not create the tag '$tag'");
+                      $this->debugmsg(print_r($check),1);
                   }
-                  print "$tagid\n";
+                  $this->debugmsg("$tagid");
               } else {
                   $tagid = $check['term_id'];
-                  print "  '$solotag' finns redan som tag med id   '$tagid'\n<br>";
+                  $this->debugmsg("  '$solotag' finns redan som tag med id   '$tagid'");
               }
               
-              print "Tagid: '$tagid' '$taxonomy'<br>\n";
-              print "Taggar ihop objekt '$id' med tag '$tagid' med taxonomy '$taxonomy'<br>\n";
+              $this->debugmsg("Tagid: '$tagid' '$taxonomy'");
+              $this->debugmsg("Taggar ihop objekt '$id' med tag '$tagid' med taxonomy '$taxonomy'");
               $blah = array($solotag);
               
-              print_r($blah);
+              $this->debugmsg(print_r($blah,1));
               $ret = wp_set_object_terms($id, $blah, $taxonomy, true);
               if (is_wp_error($ret)) {
-                  echo "ERRROR: " . $return ->get_error_message() . "<br>\n";
-                  print_r($ret);
+                  $this->debugmsg("ERRROR: " . $return ->get_error_message());;
+                  $this->debugmsg(print_r($ret),1);
               } else {
-                  echo "OK? \n<br>";
-                  print_r($ret);
+                  $this->debugmsg("OK?");
+                  $this->debugmsg(print_r($ret),1);
               }
           }
       }
   }
+  
+}
 ?>
